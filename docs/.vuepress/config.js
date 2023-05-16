9285b2221e11d71f206aeee9be3d3c9bdd82f92f
ErwinFeng <xfsy_2015@163.com>
2023-05-16 14:54:50 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,52 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { searchPlugin } from '@vuepress/plugin-search'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '博客',
+  description: '技术博客',
+  head: [['link', { rel: 'icon', href: '/images/logo/favicon.ico' }]],
+  theme: defaultTheme({
+    logo: '/images/logo/logo-512x512.png',
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '指南',
+        link: '/guide/',
+      },
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          text: '指南',
+          children: [
+            '/guide/',
+            '/guide/getting-started.md',
+          ],
+        },
+      ],
+    },
+    contributorsText: '贡献者',
+    lastUpdatedText: '最后更新时间',
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+    repo: 'https://github.com/fengwenyi/blog',
+    editLinkText: '编辑',
+    docsDir: 'docs'
+  }),
+  plugins: [
+    searchPlugin({
+      locales: {
+        '/': {
+          placeholder: '搜索',
+        },
+      },
+    }),
+  ],
+})
---
+++ docs/README.md
@@ -0,0 +1,20 @@
+---
+home: true
+heroImage: /images/logo/JavaLib-main.svg
+heroImageDark: /images/logo/JavaLib-main-dark.svg
+actions:
+  - text: 快速上手
+    link: /guide/getting-started.md
+    type: primary
+  - text: 项目简介
+    link: /guide/
+    type: secondary
+features:
+  - title: 简洁
+    details: 纯工具类，不涉及业务代码。
+  - title: 实用
+    details: 提供的每一个工具类，每一个方法，都是在实际项目中用到的。
+  - title: 一直在维护
+    details: 该项目已经维护了五年了，还会继续维护下去。
+footer: Copyright © 2023 Erwin Feng
+---
